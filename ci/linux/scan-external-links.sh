bd16a65b45216d16908373bf4c39814018dc9024
Antoine Beauchamp <end2endzone@gmail.com>
2025-09-30 18:59:31 -0400

    Updated scanner scripts to launch python web server in background and then run lychee.

--- ci/linux/scan-external-links.sh
+++ ci/linux/scan-external-links.sh
@@ -4,11 +4,36 @@
 cd "$(dirname "$0")"
 cd ../..
 
-cd public
+# Start python3 web server in a background process
+echo "Starting web server..."
+./ci/linux/run_site.sh & BG_PID=$!
+sleep 5
+echo "done."
+echo "Web server's pid is $BG_PID." 
+echo
 
-# Checking external links in files.
-
+echo "Validating with lychee..."
+# Checking all links in files.
+# Since we already validated local links,
+# we are actually only checking external links.
+# External links errors must be considerate as warnings, not errors.
 lychee \
 --verbose \
 --include-fragments \
 "http://localhost:8000"
+
+# Capture lychee exit code
+scan_result=$?
+
+echo "done."
+echo "lychee's exit code: $scan_result."
+echo
+
+# Terminate running site background process
+echo "Terminating pid $BG_PID..." 
+kill -- -$BG_PID 2>/dev/null || true; kill "$BG_PID" 2>/dev/null || true; wait "$BG_PID" 2>/dev/null || true
+echo "done."
+echo
+
+# Exit disregarding lychess's previous errors.
+exit 0
--- ci/linux/scan-local-links.sh
+++ ci/linux/scan-local-links.sh
@@ -4,15 +4,21 @@
 cd "$(dirname "$0")"
 cd ../..
 
-cd public
+# Start python3 web server in a background process
+echo "Starting web server..."
+./ci/linux/run_site.sh & BG_PID=$!
+sleep 5
+echo "done."
+echo "Web server's pid is $BG_PID." 
+echo
 
+echo "Validating with lychee..."
 # Checking local links in files. We only want to check local links
 # and not look at the links pointing to the internet.
 # The strategy is to exclude all links and then
 # manually include links to http://localhost:8000 (which is the public directory 
 # temporary hosted by a python web server) and links to
 # https://end2endzone.github.io/ (some links are hardcoded).
-
 lychee \
 --verbose \
 --include-fragments \
@@ -20,3 +26,19 @@ lychee \
 --include '^http?://localhost:8000/' \
 --include '^https?://end2endzone\.github\.io/' \
 "http://localhost:8000"
+
+# Capture lychee exit code
+scan_result=$?
+
+echo "done."
+echo "lychee's exit code: $scan_result."
+echo
+
+# Terminate running site background process
+echo "Terminating pid $BG_PID..." 
+kill -- -$BG_PID 2>/dev/null || true; kill "$BG_PID" 2>/dev/null || true; wait "$BG_PID" 2>/dev/null || true
+echo "done."
+echo
+
+# Exit with lychee's exit code
+exit $scan_result
